3e034b6baf39dfd14934611584d9375db3af486d
Ishan Pathak <ishanpathak@Ishans-MacBook-Pro.local>
2022-12-03 23:12:07 +0530

    UI update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
 import "./App.css";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider ,Box} from '@chakra-ui/react'
 import {React ,useState} from 'react';
 import { Container } from "react-bootstrap";
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+import hand from './Saly-8.png';
 
     function App() {
       const [inputList, setinputList]= useState([{Address:'', Share:''}]);
-      const handlesharechange=(e, index)=>{
-        
-        const {name, value}= e.target;  
-        if(parseInt(value) > 0 && parseInt(value) <= 100) {
-        const list= [...inputList];
-        const pair = {Address: name, Share: value};
-        list.push(pair);
-        e.preventDefault();
-        setinputList(list);
-        console.log(list);
-        }
-      }
+
       
 
       const handleinputchange=(e, index)=>{
-        
-        const {name, value}= e.target;  
-        if(parseInt(value) > 0 && parseInt(value) <= 100) {
+        const {name, value}= e.target;
         const list= [...inputList];
-        const pair = {Address: name, Share: value};
-        list.push(pair);
-        e.preventDefault();
+        list[index][name]= value;
         setinputList(list);
-        console.log(list);
-        }
+        console.log(list)
+    
       }
+      
      
       const handleremove= index=>{
         const list=[...inputList];
@@ -43,48 +32,57 @@ import { Button, ButtonGroup } from '@chakra-ui/react'
         setinputList([...inputList, { Address:'', Share:''}]);
       }
 
-      const deployContracts = () => {
-        console.log("list", inputList)
-      }
 
       return (
-        <ChakraProvider>
-        <Container className="content">
-         <div className="row">
-           <div className="col-sm-12">
-             <h5 className="mt-3 mb-4 fw-bold">Add all the addresses here </h5>
+        <ChakraProvider >
+        
+         <div className="bgmaate">
+          
+         
+          
+          <h1 className="Title">SnapSplit</h1>
+          
+
+          
+          <div className="content">
+          
+          
+          
+     <div className="row">
+       <div className="col-sm-12">
+         <h5 className="AddressHeader">Add all the address here </h5>
+           
+            { 
+            inputList.map( (x,i)=>{
+              return(
+              <div className="row mb-3">
+                 <div >
+                 <Text className="AddressTitle" color="white" fontSize='2xl'>Address</Text>
+                 <Input bg="white" width="1000px" placeholder='Enter Address' size='lg' onChange={ e=>handleinputchange(e,i)}/>
+               </div>
+               <div class="sharediv">
+               <Text color="white" fontSize='2xl'>Share</Text>
+               <Input bg="white" width="1000px"  placeholder='Enter Share' size='lg' onChange={ e=>handleinputchange(e,i)}/>
+               </div>
+              <div></div>
+               <div class="form-group col-md-2 mt-4">
+               {
+                  inputList.length!==1 &&
+                  <Button m={[2]} colorScheme='red' onClick={()=> handleremove(i)}>Remove</Button>
+               }
+               { inputList.length-1===i &&
+               <Button m={[2]}  onClick={ handleaddclick} colorScheme='whatsapp'>Add Participants</Button>
+               }
+               </div>
+            </div>
+              );
+             } )} 
+
                
-                { 
-                inputList.map( (x,i)=>{
-                  return(
-                  <div className="row mb-3">
-                     <div class="form-group col-md-4">
-                     <label >Address</label>
-                      <input type="text"  name="Address" class="form-control"  placeholder="Address" onChange={ e=>handleinputchange(e,i)} />
-                   </div>
-                   <div class="form-group col-md-4">
-                   <label >Share</label>
-                      <input type="text"  class="form-control" name="share"  placeholder="Share" onChange={ e=>handlesharechange(e,i) }/>
-                   </div>
-                   <div class="form-group col-md-2 mt-4">
-                   {
-                      inputList.length!==1 &&
-                       <Button colorScheme='red' onClick={()=> handleremove(i)}>Remove</Button>
-                   }
-                   { inputList.length-1===i &&
-                   <Button onClick={ handleaddclick} colorScheme='whatsapp'>Add Participants</Button>
-                   }
-                   </div>
-                   <Button onClick = {deployContracts}>Create Bill</Button>
-                </div>
-                
-                  );
-                 } )} 
-    
-                   
-           </div>
-         </div>
-        </Container>
+       </div>
+     </div>
+    </div>
+    </div>
         </ChakraProvider>
       );
 
