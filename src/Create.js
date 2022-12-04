05eac15e62052b52031a78624f3d0c9ad4e50dbf
vrajdesai78 <vrajdesai78@gmail.com>
2022-12-04 08:00:42 +0530

    New UI updated

---
+++ src/Create.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+function App() {
+
+  const [inputList, setInputList] = useState([{ firstName: "", lastName: "" }]);
+
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { firstName: "", lastName: "" }]);
+  };
+
+  if (window.ethereum) {
+    // Do something 
+  } else {
+    alert("install metamask extension!!")
+  }
+  
+  const web3 = new Web3(Web3.givenProvider || "http:// localhost:8545");
+
+  async function connect() {
+    if (window.ethereum) {
+
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      window.web3 = new Web3(window.ethereum);
+
+    } else {
+      console.log("No wallet");
+    }
+  }
+
+  return (
+    <div className="App">
+      {inputList.map((x, i) => {
+        return (
+          <div className="box">
+            <button style={{ align: "right" }} onClick={connectWallet}>Connect Wallet</button>
+            <input
+              name="firstName"
+              placeholder="Enter First Name"
+              value={x.firstName}
+              onChange={e => handleInputChange(e, i)}
+            />
+            <input
+              className="ml10"
+              name="lastName"
+              placeholder="Enter Last Name"
+              value={x.lastName}
+              onChange={e => handleInputChange(e, i)}
+            />
+            <div className="btn-box">
+              {inputList.length !== 1 && <button
+                className="mr10"
+                onClick={() => handleRemoveClick(i)}>Remove</button>}
+              {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+            </div>
+          </div>
+        );
+      })}
+      <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
+    </div>
+  );
+}
+
+export default App;
